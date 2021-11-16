013cc3325d511928f8657e7f9016f952e5340b3c
Tareq Hasan <tareq1988@gmail.com>
2021-11-16 09:17:18 +0300

    authorization

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,16 +2,47 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Auth Service!')
-})
+const publicUris = [
+  '/products/:id'
+];
 
-app.get('/users', (req, res) => {
+app.get('/auth/users', (req, res) => {
   res.send('User list')
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/auth/users/:id', (req, res) => {
   res.send('single user: ' + req.params.id)
+})
+
+app.post('/auth/_check', (req, res) => {
+  if (!req.headers.authorization) {
+    res.status(401).send('Missing authorization header');
+  }
+
+  let originalUri = req.headers['x-original-uri'];
+  console.log(req.headers);
+  // console.log(originalUri);
+
+  const parts = req.headers.authorization.split(' ');
+
+  if (parts.length !== 2) {
+    return res.status(403).json({ error: 'Authentication format is Authorization: Bearer [token]' });
+  }
+
+  let scheme = parts[0];
+  let token = parts[1];
+
+  // console.log(scheme, token);
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(403).json({ error: 'Authentication format is Authorization: Bearer [token]' });
+  }
+
+  if (token === 'valid-auth') {
+    return res.send('Authorized');
+  }
+
+  return res.status(401).send('Authorization failed');
 })
 
 app.listen(port, () => {
--- catalog-service/index.js
+++ catalog-service/index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Catalog Service!')
-})
-
 app.get('/products', (req, res) => {
   res.send('List of products');
 })
